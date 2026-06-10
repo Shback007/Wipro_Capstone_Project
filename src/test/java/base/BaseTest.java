d02b4389acc86b10cc480cf8cfe286b0a3b70617
Sagnik Hore <sagnikhoredht@gmail.com>
2026-06-10 17:36:47 +0530

    basetest Update

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,9 +1,7 @@
 package base;
 
 import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
 import org.apache.logging.log4j.LogManager;
