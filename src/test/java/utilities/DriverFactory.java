68ef8ac62b4a7086e942bff598cd23b7b2b05224
Sagnik Hore <sagnikhoredht@gmail.com>
2026-06-12 01:24:05 +0530

    Docker-Integreation

--- src/test/java/utilities/DriverFactory.java
+++ src/test/java/utilities/DriverFactory.java
@@ -1,13 +1,18 @@
 package utilities;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
+
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class DriverFactory {
 
@@ -15,66 +20,134 @@ public class DriverFactory {
 
     private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
+
+
     // Initialize Driver
 
     public static WebDriver initializeDriver() {
 
         String browser = ConfigReader.getProperty("browser");
-        
+
         String headless = ConfigReader.getProperty("headless");
 
-        switch(browser.toLowerCase()) {
+        String execution_env = ConfigReader.getProperty("execution_env");
 
-        case "chrome":
-        	
-        	ChromeOptions chromeOptions = new ChromeOptions();
-        	
-        	if(headless.equalsIgnoreCase("true")) {
-        		
-        		chromeOptions.addArguments("--headless=new");
-        		chromeOptions.addArguments("--disable-gpu");
-        		chromeOptions.addArguments("--window-size=1920,1080");
 
-        		chromeOptions.addArguments("--remote-allow-origins=*");
-        		chromeOptions.addArguments("--no-sandbox");
-        		chromeOptions.addArguments("--disable-dev-shm-usage");
-        		chromeOptions.addArguments("--disable-extensions");
-        		chromeOptions.addArguments("--disable-popup-blocking");
-        	}
-        	
-            driver.set(new ChromeDriver(chromeOptions));
 
-            break;
+        try {
 
-        case "edge":
-        	
-        	EdgeOptions edgeOptions = new EdgeOptions();
-        	
-        	if(headless.equalsIgnoreCase("true")) {
-        		
-        		edgeOptions.addArguments("--headless=new");
-        		edgeOptions.addArguments("--disable-gpu");
-        		edgeOptions.addArguments("--window-size=1920,1080");
+            switch(browser.toLowerCase()) {
 
-        		edgeOptions.addArguments("--remote-allow-origins=*");
-        		edgeOptions.addArguments("--no-sandbox");
-        		edgeOptions.addArguments("--disable-dev-shm-usage");
-        		edgeOptions.addArguments("--disable-extensions");
-        		edgeOptions.addArguments("--disable-popup-blocking");
-        	}
+            case "chrome":
 
-            driver.set(new EdgeDriver(edgeOptions));
+                ChromeOptions chromeOptions =
+                        new ChromeOptions();
 
-            break;
 
-        default:
+                // Headless Configuration
 
-            throw new RuntimeException("Invalid Browser : " + browser);
+                if(headless.equalsIgnoreCase("true")) {
+
+                    chromeOptions.addArguments("--headless=new");
+
+                    chromeOptions.addArguments("--disable-gpu");
+
+                    chromeOptions.addArguments("--window-size=1920,1080");
+
+                    chromeOptions.addArguments("--remote-allow-origins=*");
+
+                    chromeOptions.addArguments("--no-sandbox");
+
+                    chromeOptions.addArguments("--disable-dev-shm-usage");
+
+                    chromeOptions.addArguments("--disable-extensions");
+
+                    chromeOptions.addArguments("--disable-popup-blocking");
+                }
+
+
+
+                // Local Execution
+
+                if(execution_env.equalsIgnoreCase("local")) {
+
+                    driver.set( new ChromeDriver(chromeOptions));
+                }
+
+
+                // Remote Execution
+
+                else if(execution_env.equalsIgnoreCase("remote")) {
+
+                    driver.set(new RemoteWebDriver(new URL("http://selenium:4444/wd/hub"), chromeOptions));
+                }
+
+                break;
+
+
+
+            case "edge":
+
+                EdgeOptions edgeOptions = new EdgeOptions();
+
+
+                // Headless Configuration
+
+                if(headless.equalsIgnoreCase("true")) {
+                	
+                    edgeOptions.addArguments("--headless=new");
+
+                    edgeOptions.addArguments("--disable-gpu");
+
+                    edgeOptions.addArguments("--window-size=1920,1080");
+
+                    edgeOptions.addArguments("--remote-allow-origins=*");
+
+                    edgeOptions.addArguments("--no-sandbox");
+
+                    edgeOptions.addArguments("--disable-dev-shm-usage");
+
+                    edgeOptions.addArguments("--disable-extensions");
+
+                    edgeOptions.addArguments("--disable-popup-blocking");
+                }
+
+
+
+                // Local Execution
+
+                if(execution_env.equalsIgnoreCase("local")) {
+
+                    driver.set(new EdgeDriver(edgeOptions));
+                }
+
+
+                // Remote Execution
+
+                else if(execution_env.equalsIgnoreCase("remote")) {
+
+                    driver.set(new RemoteWebDriver(new URL("http://selenium:4444/wd/hub"), edgeOptions));
+                }
+
+                break;
+
+
+
+            default:
+
+                throw new RuntimeException("Invalid Browser : " + browser);
+            }
+
         }
 
+        catch(MalformedURLException e) {
+
+            e.printStackTrace();
+        }
+
+       // Common Browser Configuration
 
         getDriver().manage().window().maximize();
-
 
         getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
@@ -82,11 +155,17 @@ public class DriverFactory {
     }
 
 
+
+    // Get Driver
+
     public static WebDriver getDriver() {
 
         return driver.get();
     }
 
+
+
+    // Quit Driver
 
     public static void quitDriver() {
 
